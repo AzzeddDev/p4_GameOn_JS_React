3175b91f87ac7361fd747285b3e0b587d4683d18
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2024-01-25 14:43:34 +0100

    update js files

--- scripts/modal.js
+++ scripts/modal.js
@@ -3,6 +3,15 @@ const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelector(".close");
 
+// Responsive Nav
+function editNav() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 // Fonction ouverture du modal
 modalBtn.forEach((btn) => btn.addEventListener("click", () => {
