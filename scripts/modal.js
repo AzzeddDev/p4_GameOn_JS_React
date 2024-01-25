ff98d02d60372e10eac00b51c51949a42803670f
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2024-01-25 14:39:48 +0100

    update, modal.js file

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,19 +1,15 @@
-// DOM Elements
-const modalbg = document.querySelector(".bground");
+// Const
+const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+// Fonction ouverture du modal
+modalBtn.forEach((btn) => btn.addEventListener("click", () => {
+  modalBg.style.display = "block";
+}));
 
 //Fonction pour fermer la modale
-function closeModal() {
-  modalbg.style.display = "none"; }
-document.querySelector('.close').addEventListener("click",closeModal);
+closeBtn.addEventListener("click", () => {
+  modalBg.style.display = "none";
+});
